9ea210e3635aed66462bf9413b13d565fc3f20d4
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 01:52:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
       :team_name => "Brooklyn_Nets",
       :colors => ["Black", "White"],
       :players => [ 
-        { {:Alan_Anderson} :player_name => "Alan_Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+        {:player_name => "Alan_Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
         {:player_name => "Reggie_Evans", :number => 30, :shoe => 14, :points => 22, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7}, 
         {:player_name => "Brook_lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15}, 
         {:player_name => "Mason_Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5}, 
