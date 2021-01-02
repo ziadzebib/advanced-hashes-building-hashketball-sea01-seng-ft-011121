912c035f2f74ee2b0483b920887db912a6330df2
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 01:43:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,8 +20,8 @@ def game_hash
       {:player_name => "DeSagna_Diop", :number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5}, 
       {:player_name => "Ben_Gordon", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0}, 
       {:player_name => "Kemba_Walker", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12}
-      ]
-    }
+       ]
+     }
   }
   return game_hash
 end
