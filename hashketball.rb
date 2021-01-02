839efa4434ebb1f32199e9a624220715e47b6cf9
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 01:36:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,10 @@
 def game_hash
   game_hash = {
     :home => {
-      :team_name => "Brooklyn_Nets"
-#      :colors => ["Black", "White"],
-#      :players => [ {:player_name => "Alan_Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {:player_name => "Reggie_Evans", :number => 30, :shoe => 14, :points => 22, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7}, {:player_name => "Brook_lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15}. {:player_name => "Mason_Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5} {:player_name => "Jason_Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
- #     ]
+      :team_name => "Brooklyn_Nets",
+      :colors => ["Black", "White"],
+      :players => [ {:player_name => "Alan_Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {:player_name => "Reggie_Evans", :number => 30, :shoe => 14, :points => 22, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7}, {:player_name => "Brook_lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15}. {:player_name => "Mason_Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5} {:player_name => "Jason_Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
+      ]
     }
     :away {
       :team_name => "Charlotte_Hornets",
