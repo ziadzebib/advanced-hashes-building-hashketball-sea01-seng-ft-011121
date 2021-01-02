49247ea368e3d2e67d5ba3068023425dfb06d5c4
Ziad Zebib <zebibziad@gmail.com>
2021-01-02 01:01:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+def game_hash
+game_hash {
+  :home {
+    :team_name => "Brooklyn_Nets"
+    :colors => ["Black", "White"]
+    :players => [:Player_name => {"Alan_Anderson"}]
+  }
+  :away {
+    :team_name => "Charlotte_Hornets"
+    :colors => ["Turquoise", "Purple"]
+    :players  
+  }
+}
